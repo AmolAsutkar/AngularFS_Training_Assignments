5781953771b95096b3b7db8c754af75d806d48c8
AmolAsutkar <asamolutkar93@gmail.com>
2022-04-28 00:42:41 +0530

    added angular output

---
+++ Angular/Day01/Login_Demo/login/login.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  
+    UserName:string = "";
+    Password:string = "";
+    result:string = "";
+    count:number = 0;
+    isDisabled:boolean = false;
+ 
+    public login_click():void
+    {
+       if(this.UserName == "admin" && this.Password == "admin123")
+       {
+         this.result ="Welcome to Admin!";
+       }
+       else
+       {
+         this.count++;
+         this.result = "Invalid username or password!"
+         if(this.count == 3)
+         {
+           this.isDisabled = true;
+         }
+       }
+    }
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
